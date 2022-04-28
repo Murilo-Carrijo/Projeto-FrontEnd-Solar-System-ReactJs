1ae7d8fcedafba2554766797a880953d705d7772
Murilo <murilocarrijoadm@gmail.com>
2022-04-28 17:06:16 -0300

    Renderização do componente Title.js no componete Missions.js

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Title from './Title';
+
 class Missions extends React.Component {
   render() {
     return (
-      <div className="contaniner-missions" data-testid="missions">.</div>
+      <div className="contaniner-missions" data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
