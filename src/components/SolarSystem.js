d482b82c078ff46811eb8ac193caf4a35e3f77ed
Murilo <murilocarrijoadm@gmail.com>
2022-04-28 15:05:34 -0300

    Implemenação do componente SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div
+        className="container-solarSystem"
+        data-testid="solar-system"
+      >
+        SolarSystem
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
