dcb6634dc219360738c38d4d90543e49f14deecc
Murilo <murilocarrijoadm@gmail.com>
2022-04-28 15:07:10 -0300

    renderização dos componente SolareSystem e Title - CSS inicial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+        <Title headline="Murilo" />
+      </div>
     );
   }
 }
